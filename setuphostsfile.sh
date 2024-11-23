09d933a7265db0ad316e498fc6f1377fd5af781a
joseburger <jose.burger@mailfence.com>
2024-11-23 07:02:11 +0100

    Update setuphostsfile.sh
    renamed files inside script

--- setuphostsfile.sh
+++ setuphostsfile.sh
@@ -9,12 +9,12 @@ rm /etc/hosts
 # Now download the new hosts file and put it into place
 curl -H 'Cache-Control: no-cache, no-store' "http://sbc.io/hosts/alternates/fakenews-gambling-porn/hosts" >> /etc/hosts
 # add my own custom entries to host file
-cat customhosts >> /etc/hosts
+cat customblocklist.txt >> /etc/hosts
 
 # create/update full hosts file for use on android via custom dns etc.
 curl -H 'Cache-Control: no-cache, no-store' "http://sbc.io/hosts/alternates/fakenews-gambling-porn/hosts" >> fullhostslist
 # add my own custom entries to host file
-cat customhosts >> fullhostslist
+cat customblocklist.txt >> fullhostslist
 
 echo "done"
 
